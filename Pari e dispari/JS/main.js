fc5d49cc4d7a90a08519ed20e2c873b3b3d13a44
Fef97 <federicakr97@gmail.com>
2023-05-10 00:36:54 +0200

    esercizio completato

--- Pari e dispari/JS/main.js
+++ Pari e dispari/JS/main.js
@@ -1,6 +1,9 @@
+// chiediamo all'utente di scegliere tra pari e dispari
+
 let inputUtente = prompt("scegli pari o dispari");
 console.log("L'utente ha scelto " + inputUtente);
 
+// chiediamo all'utente di scegliere un numero da 1 a 10
 let userNumber = parseInt(prompt("scegli un numero da 1 a 10"))
 console.log(userNumber);
 
@@ -9,8 +12,7 @@ console.log(userNumber);
 
 
 
-
-
+// aggiungiamo funzione per creare numero random pc
 let numeroPC = generaNumeroRandomPc (1,10);
 
 function generaNumeroRandomPc (min,max) {
@@ -22,15 +24,18 @@ function generaNumeroRandomPc (min,max) {
     
 }
 
-let solution = sum (numeroPC,userNumber);
+// aggiungiamo funzione per fare la somma tra numero utente e numero pc
+
+let soluzione = sum (numeroPC,userNumber);
 
 function sum (numeroPC,userNumber) {
 
-    let somma = (numeroPC + userNumber);
+    let somma =(numeroPC + userNumber);
     console.log("La somma dei due numeri è " + somma);
     
     
-
+    // aggiungiamo condizione
+    // se la somma dei numeri è pari o dispari
     if (somma % 2 === 0) {
      risultato = "pari";
      console.log(risultato);
@@ -40,7 +45,15 @@ function sum (numeroPC,userNumber) {
         console.log(risultato);
     }
 
-    return sum
+    return risultato
+}
+
+
+// dichiariamo chi ha vinto
+if (inputUtente === soluzione) {
+    console.log("hai vinto");
+} else {
+    console.log("hai perso");
 }
 
 
